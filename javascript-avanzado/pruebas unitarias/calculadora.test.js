e821451f84c7a606054d2b5c5736ecddbd7b9e3f
Jonathan Quino <jonathan.quino.26793@gmail.com>
2022-01-24 14:04:38 -0600

    Se agregan los test de sumar y restar

--- javascript-avanzado/pruebas unitarias/calculadora.test.js
+++ javascript-avanzado/pruebas unitarias/calculadora.test.js
@@ -1,9 +1,17 @@
-const sumar = require('./calculadora'); //Se pide la funcion sumar del modulo calculadora, sin extension
-describe('Pruebas en el componente calculadora',()=>{
+const {sumar} = require('./calculadora'); //Se pide la funcion sumar del modulo calculadora, sin extension
+const {restar} = require('./calculadora'); 
+describe('Pruebasde sumar 3 y 5',()=>{
 
-    Test('Prueba en la funcion sumar', () => {
-
+    test('Prueba en la funcion sumar', () => {
         expect(sumar(3,5)).toBe(8);
+    });    
+
+});  //Descripcion:Englobar todas las pruebas, hacia que archivo se hacen las pruebas
+
+describe('Pruebasde restar 5 y 3',()=>{
+
+    test('Prueba en la funcion restar', () => {
+        expect(restar(5,3)).toBe(2);
     });    
 
 });  //Descripcion:Englobar todas las pruebas, hacia que archivo se hacen las pruebas
