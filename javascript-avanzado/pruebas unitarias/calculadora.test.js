2b4afbccfd3516776062b40904a61e7878dd8aef
Jonathan Quino <jonathan.quino.26793@gmail.com>
2022-01-25 13:47:01 -0600

    Se agregan ejercicios de pruebas unitarias

--- javascript-avanzado/pruebas unitarias/calculadora.test.js
+++ javascript-avanzado/pruebas unitarias/calculadora.test.js
@@ -1,17 +1,22 @@
-const {sumar} = require('./calculadora'); //Se pide la funcion sumar del modulo calculadora, sin extension
-const {restar} = require('./calculadora'); 
-describe('Pruebasde sumar 3 y 5',()=>{
+const {sumar,restar,multiplicar,dividir} = require('./calculadora'); //Se pide la funcion sumar del modulo calculadora, sin extension
+
+describe('Pruebas de calcauladora',()=>{
 
     test('Prueba en la funcion sumar', () => {
         expect(sumar(3,5)).toBe(8);
     });    
 
-});  //Descripcion:Englobar todas las pruebas, hacia que archivo se hacen las pruebas
-
-describe('Pruebasde restar 5 y 3',()=>{
-
     test('Prueba en la funcion restar', () => {
         expect(restar(5,3)).toBe(2);
     });    
 
+    test('Prueba de la funcion multiplicar', () =>{
+        expect(multiplicar(5,2)).toBe(10);
+    });
+
+    test('Prueba de la funcion dividir', () =>{
+        expect(dividir(10,2)).toBe(5);
+    });
+
 });  //Descripcion:Englobar todas las pruebas, hacia que archivo se hacen las pruebas
+
